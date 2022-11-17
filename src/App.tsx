7b1861169780d360f07531abe22d543980686817
AVANADE-CORP\r.bajor <r.bajor@avanade.com>
2022-11-17 17:11:51 +0100

    added rest

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,34 @@ function App() {
     { name: "5Hs7AfiMXgs", playerVars: { start: 248 } },
   ];
 
+  const list2 = [
+    { name: "t4Zz6-aG8Iw", playerVars: { start: 31 } },
+    { name: "OjcN6RzWFWs", playerVars: { start: 1234 } },
+    { name: "OjcN6RzWFWs", playerVars: { start: 1296 } },
+    { name: "0vdQveRyVic" },
+    { name: "1YT30fCSsLo" },
+  ];
+
+  const list3 = [
+    { name: "lPKRiU9u_Hc", playerVars: { start: 1062 } },
+    { name: "eA8DuVW3vPo" },
+  ];
+
   return (
     <div className="App">
       {list.map((x) => (
+        <div key={x.name}>
+          <YouTube videoId={x.name} opts={{ playerVars: x?.playerVars }} />
+        </div>
+      ))}
+      <hr />
+      {list2.map((x) => (
+        <div key={x.name}>
+          <YouTube videoId={x.name} opts={{ playerVars: x?.playerVars }} />
+        </div>
+      ))}
+      <hr />
+      {list3.map((x) => (
         <div key={x.name}>
           <YouTube videoId={x.name} opts={{ playerVars: x?.playerVars }} />
         </div>
